04af30093ea0cf94d1a2e51e92e08e29800ccfba
wangjingjing <wangjingjing@df0d99a4-deb6-4343-9628-55602dcec4b6>
2014-12-11 15:03:04 +0000

    添加辅导员选择班级功能

--- dormitory/WebRoot/js/f_index.js
+++ dormitory/WebRoot/js/f_index.js
@@ -4,7 +4,6 @@
  *
  */
 $(function(){ 
-    var status=0;
 	$(".stu li:eq(0)").click(function(){
 		$("#base_right").empty().load("f_modify.html");
 	});
@@ -17,23 +16,27 @@ $(function(){
    $(".stuqin li:eq(1)").click(function(){
 		$("#base_right").empty().load("f_modifystuchangehotel.html");
 	});
-	$(".wait li:first-child").click(function(){
+   $(".classes li:eq(0)").click(function(){
 		$("#base_right").empty();
-		$.messager.alert("提示","此部分功能还在开发","info");
+		$("#base_right").empty().load("f_viewclass.html");
+	});
+   $(".classes li:eq(1)").click(function(){
+		$("#base_right").empty();
+		$("#base_right").empty().load("f_addclassl.html");
 	});	
 	//树形控件点击添加事件，用于指示当前页面
-	$(".stu li:eq(0),.stu li:eq(1),.stuqin li:eq(0),.stuqin li:eq(1)").hover(function(){
+	$(".stu li:eq(0),.stu li:eq(1),.stuqin li:eq(0),.stuqin li:eq(1),.classes li:eq(0),.classes li:eq(1)").hover(function(){
 		var status=0;
 		$(this).click(function(){
 			if(status==0){
 				$("li").removeClass("hover");
 			    $(this).addClass("hover");
 			    status=1;
-				}
+			}
 	    	else if(status==1){
 			  $(this).removeClass("hover");
 			  status=0;
 			}
 		});
-	})
-})
+	});
+});
